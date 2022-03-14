711f27eb05e2a3cfa116f6cf9fb2080994f0a048
Jason G <jasondouglasgibbs@gmail.com>
2022-03-14 16:21:22 -0400

    -Added error checking and file list creation.

--- UpdateScript.R
+++ UpdateScript.R
@@ -3,8 +3,14 @@
 ##Libraries##
 library(Microsoft365R)
 library(tidyverse)
+library(readxl)
 `%!in%`<-Negate(`%in%`)
 
+##Inputs
+FileSpreadsheet<-read_xlsx("LocalFileList.xlsx")
+
+
+##Initial OneDrive object creation.##
 OneDrive<-get_personal_onedrive(auth_type="device_code")
 
 TopLevelList<-OneDrive$list_files(full_names=TRUE)
@@ -106,6 +112,20 @@ for(i in 1:nrow(NotTopLevelFolderList)){
 
 }
 
+##Tests to see if all folders are included.
+TestResults<-TopLevelFolderList
+TestResults<-TestResults[0,]
+for(i in 1:nrow(NotTopLevelFolderList)){
+  Test<-OneDrive$list_files(NotTopLevelFolderList[i, "name"], full_names=TRUE)
+  Test<-filter(Test, size!=0)
+  Test<-filter(Test, isdir==TRUE)
+  Test<-filter(Test, Test[,"name"]%!in%FolderList[, "name"])
+  if(nrow(Test)!=0){
+  TestResults<-rbind(TestResults,Test)
+  }
+  print(i)
+}
+
 
 ##Builds the initial OneDrive file list.##
 FileList<-OneDrive$list_files(full_names=TRUE)
@@ -134,5 +154,180 @@ for(i in 1:nrow(FolderList)){
   }
   FileList<-rbind(FileList, FileListBind)
   
+}
+
+
+
+##Local Folder/File portion.##
+FileSpreadsheet<-read_xlsx("LocalFileList.xlsx")
+
+FoldersToCreate<-FileSpreadsheet
+
+
+for (i in 1:nrow(FoldersToCreate)){
+  if(as.character(FoldersToCreate[i,"Notes"])=="FilesIn"&!is.na(FoldersToCreate[i,"Notes"])){
+    next
+  }
+  
+  FoldersToBind<-as.data.frame(list.dirs(as.character(FoldersToCreate[i,"Path"])))
+  if(nrow(FoldersToBind)>1){
+  FoldersToBind<-as.data.frame(FoldersToBind[-c(1),])
+  }
+  names(FoldersToBind)<-"Path"
+  NotesName<-"Notes"
+  FoldersToBind<-add_column(FoldersToBind, !!NotesName:=NA_character_)
+  FoldersToBind<-filter(FoldersToBind,FoldersToBind[,"Path"]%!in%FoldersToCreate[, "Path"] )
+  FoldersToCreate<-rbind(FoldersToCreate, FoldersToBind)
+  
+  
+}
+
+OneDrivePath<-"OneDrivePath"
+FoldersToCreate<-add_column(FoldersToCreate, !!OneDrivePath:=NA_character_)
+
+
+for(i in 1:nrow(FoldersToCreate)){
+  if(grepl("#",as.character(FoldersToCreate[i,"Path"]))){
+    NewFileName<-str_replace_all(as.character(FoldersToCreate[i,"Path"]), pattern="#","")
+    file.rename(from=as.character(FoldersToCreate[i,"Path"]),
+                str_replace_all(as.character(FoldersToCreate[i,"Path"]), pattern="#",""))
+    
+    FoldersToCreate[i,"Path"]<-NewFileName
+    print(paste0("Folder name changed at ", i, " row."))
+  }
+  
+  if(grepl("%",as.character(FoldersToCreate[i,"Path"]))){
+    NewFileName<-str_replace_all(as.character(FoldersToCreate[i,"Path"]), pattern="%","")
+    file.rename(from=as.character(FoldersToCreate[i,"Path"]),
+                str_replace_all(as.character(FoldersToCreate[i,"Path"]), pattern="%",""))
+    
+    FoldersToCreate[i,"Path"]<-NewFileName
+    print(paste0("Folder name changed at ", i, " row."))
+  }
+  FoldersToCreate[i, "OneDrivePath"]<-substring(as.character(FoldersToCreate[i, "Path"]),4)
+  
+}
+
+##Add subset logic for folders below.##
+
+FoldersToCreateTest<-FoldersToCreate
+FoldersToActuallyCreate<-FoldersToCreate
+RemovalList<-list()
+
+
+for(i in 1:nrow(FoldersToCreateTest)){
+  if(FoldersToCreateTest[i,"OneDrivePath"]%in%FolderList[, "name"]){
+    RemovalListAdd<-i
+    RemovalList<-rbind(RemovalList,RemovalListAdd)
+  }
+  
+
+}
+
+RemovalList<-sapply(RemovalList,"[[",1)
+FoldersToActuallyCreate<-FoldersToActuallyCreate[-RemovalList,]
+FoldersToCreate<-FoldersToActuallyCreate
+
+
+
+for(i in 1:nrow(FoldersToCreate)){
+  FolderPath<-as.character(FoldersToCreate[i,"OneDrivePath"])
+  FolderCreateTry<-try(OneDrive$create_folder(FolderPath), silent=TRUE)
+  
+  if(class(FolderCreateTry)=="try-error"){
+    error_type <- attr(FolderCreateTry,"condition")
+    error_message<-error_type$message
+
+    if(grepl("with the same name", error_message)){
+      print("Folder already exists (typically an empty folder, skipping to next")
+      print(i)
+      next
+      
+    }
+      
+  }
+  
+  print(i)
+}
+
+
+
+
+##Files to upload
+FilesToUpload<-FoldersToCreate
+FilesToUpload<-FilesToUpload[0,]
+
+for(i in 1:nrow(FoldersToCreate)){
+  if(as.character(FoldersToCreate[i,"Notes"])=="FilesIn"&!is.na(FoldersToCreate[i,"Notes"])){
+    FilesToUploadBind<-as.data.frame(setdiff(list.files(as.character(FoldersToCreate[i,"Path"]), full.names = TRUE), list.dirs(as.character(FoldersToCreate[i,"Path"]),recursive = FALSE, full.names = TRUE)))
+    names(FilesToUploadBind)<-"Path"
+    NotesName<-"Notes"
+    FilesToUploadBind<-add_column(FilesToUploadBind, !!NotesName:=NA_character_)
+    FilesToUploadBind<-add_column(FilesToUploadBind, !!OneDrivePath:=NA_character_)
+    FilesToUpload<-rbind(FilesToUpload, FilesToUploadBind)
+    next
+    }
+  
+  # if(as.character(FoldersToCreate[i,"Notes"])=="AllFiles"&!is.na(FoldersToCreate[i,"Notes"])){
+  #   FilesToUploadBind<-as.data.frame(setdiff(list.files(as.character(FoldersToCreate[1,"Path"]), full.names = TRUE), list.dirs(as.character(FoldersToCreate[1,"Path"]),recursive = FALSE, full.names = TRUE)))
+  #   names(FilesToUploadBind)<-"Path"
+  #   NotesName<-"Notes"
+  #   FilesToUploadBind<-add_column(FilesToUploadBind, !!NotesName:=NA_character_)
+  #   FilesToUploadBind<-add_column(FilesToUploadBind, !!OneDrivePath:=NA_character_)
+  #   FilesToUpload<-rbind(FoldersToCreate, FoldersToBind)
+  #   next
+  # }
+  
+  FilesToUploadBind<-as.data.frame(setdiff(list.files(as.character(FoldersToCreate[i,"Path"]), full.names = TRUE), list.dirs(as.character(FoldersToCreate[i,"Path"]),recursive = FALSE, full.names = TRUE)))
+  names(FilesToUploadBind)<-"Path"
+  NotesName<-"Notes"
+  FilesToUploadBind<-add_column(FilesToUploadBind, !!NotesName:=NA_character_)
+  FilesToUploadBind<-add_column(FilesToUploadBind, !!OneDrivePath:=NA_character_)
+  
+  FilesToUpload<-rbind(FilesToUpload, FilesToUploadBind)
+
+}
+
+for(i in 1:nrow(FilesToUpload)){
+  FilesToUpload[i, "OneDrivePath"]<-substring(as.character(FilesToUpload[i, "Path"]),4)
+  
+}
+
+TotalFiles<-as.numeric(nrow(FilesToUpload))
+for(i in 1:nrow(FilesToUpload)){
+  if(grepl("#",as.character(FilesToUpload[i,"Path"]))){
+    NewFileName<-str_replace_all(as.character(FilesToUpload[i,"Path"]), pattern="#","")
+    file.rename(from=as.character(FilesToUpload[i,"Path"]),
+                str_replace_all(as.character(FilesToUpload[i,"Path"]), pattern="#",""))
+    
+    FilesToUpload[i,"Path"]<-NewFileName
+    FilesToUpload[i, "OneDrivePath"]<-substring(NewFileName,4)
+    print(paste0("File name changed at ", i, " row."))
+  }
+  
+  if(grepl("%",as.character(FilesToUpload[i,"Path"]))){
+    NewFileName<-str_replace_all(as.character(FilesToUpload[i,"Path"]), pattern="%","")
+    file.rename(from=as.character(FilesToUpload[i,"Path"]),
+                str_replace_all(as.character(FilesToUpload[i,"Path"]), pattern="%",""))
+    
+    FilesToUpload[i,"Path"]<-NewFileName
+    FilesToUpload[i, "OneDrivePath"]<-substring(NewFileName,4)
+    print(paste0("File name changed at ", i, " row."))
+  }
+  
+  
+  FileUploadTry<-try(OneDrive$upload_file(src=as.character(FilesToUpload[i,"Path"]), dest=as.character(FilesToUpload[i,"OneDrivePath"])))
+  
+    if(class(FileUploadTry)=="try-error"){
+      error_type <- attr(FileUploadTry,"condition")
+      error_message<-error_type$message
+      print(class(error_type))
+      print(error_type$message)
+      break
+
+    }
+  
+  
+  print(paste0(i, " files uploaded of ", TotalFiles, " total."))
 }
 
